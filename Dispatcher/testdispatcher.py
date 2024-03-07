6500e1f3b094ee5fb3ad6bf40223a22814336964
Yisaterese <teresejosephyisa@gmail.com>
2024-03-07 09:15:04 +0100

    added code

--- Dispatcher/testdispatcher.py
+++ Dispatcher/testdispatcher.py
@@ -1,4 +1,7 @@
+import pytest
+
 from Dispatcher.dispatchrider import calculate_riders_wage
+from Dispatcher.invaliderror import InvalidNumberOfDelivery
 
 
 def test_calculate_riders_wages_for_25_delivery():
@@ -7,3 +10,17 @@ def test_calculate_riders_wages_for_25_delivery():
 
 def test_calculate_riders_wages_for_80_delivery():
     assert calculate_riders_wage(80) == 45000
+
+
+def test_calculate_riders_wages_for_60_delivery():
+    assert calculate_riders_wage(60) == 20000
+
+
+def test_calculate_riders_wages_for_70_delivery():
+    assert calculate_riders_wage(70) == 40000
+
+
+def test_calculate_riders_wages_above_raise_error():
+    with pytest.raises( InvalidNumberOfDelivery) as info:
+        calculate_riders_wage(188)
+    assert str(info.value) == "number of delivery above range"
