b6f760e8098e2d2c674e420f8ca6fed5d9b7f60b
Yisaterese <teresejosephyisa@gmail.com>
2024-03-07 09:02:57 +0100

    added code

--- Dispatcher/testdispatcher.py
+++ Dispatcher/testdispatcher.py
@@ -1,6 +1,9 @@
-
 from Dispatcher.dispatchrider import calculate_riders_wage
 
 
 def test_calculate_riders_wages_for_25_delivery():
     assert calculate_riders_wage(25) == 9000
+
+
+def test_calculate_riders_wages_for_80_delivery():
+    assert calculate_riders_wage(80) == 45000
