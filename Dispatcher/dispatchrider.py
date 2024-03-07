e23a984733501a170cf93b23afcc698f24dffcdc
Yisaterese <teresejosephyisa@gmail.com>
2024-03-07 09:18:28 +0100

    added code

--- Dispatcher/dispatchrider.py
+++ Dispatcher/dispatchrider.py
@@ -4,12 +4,12 @@ from Dispatcher.invaliderror import InvalidNumberOfDelivery
 def calculate_riders_wage(number_of_delivery):
     if number_of_delivery < 50:
         return number_of_delivery * 160 + 5000
-    elif  number_of_delivery < 60:
+    elif number_of_delivery < 60:
         return number_of_delivery * 200 + 5000
 
     elif number_of_delivery < 70:
         return number_of_delivery * 250 + 5000
-    elif number_of_delivery  >= 70:
+    elif number_of_delivery >= 70:
         return number_of_delivery * 500 + 5000
-    elif 1 > number_of_delivery > 100:
+    elif number_of_delivery < 1 and number_of_delivery > 100:
         raise InvalidNumberOfDelivery("number of delivery above range")
