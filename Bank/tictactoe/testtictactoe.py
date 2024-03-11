49128a9487484936e157920bcd85e36dcde7cac3
Yisaterese <teresejosephyisa@gmail.com>
2024-03-11 14:11:12 +0100

    added code

--- Bank/tictactoe/testtictactoe.py
+++ Bank/tictactoe/testtictactoe.py
@@ -1,6 +1,16 @@
 from Bank.tictactoe.tictactoe import TicTacToe
+from Bank.tictactoe.cellvalues import CellValues
 
 
 def test_the_number_of_cell_in_board_cells():
     tictactoe = TicTacToe()
-    assert tictactoe.get_number_of_cells() == 3
+    assert tictactoe.get_number_of_cells() == 9
+
+
+def test_cellValue_picked_equals_the_enum_type():
+    tictactoe = TicTacToe()
+    print(tictactoe.get_number_of_cells())
+    assert tictactoe.pickCell(3, CellValues.X) is CellValues.X
+
+
+
--- Bank/tictactoe/tictactoe.py
+++ Bank/tictactoe/tictactoe.py
@@ -1,4 +1,5 @@
 from Bank.tictactoe.cellvalues import CellValues
+from Bank.tictactoe.invalidBordCellException import InvalidBordCellException
 
 
 class TicTacToe:
@@ -6,9 +7,29 @@ class TicTacToe:
         self.board_cells = [[], [], []]
 
     def populate_board_cells(self):
-        for row in range(0,3):
+        for row in range(0, 3):
             for col in range(0, 3):
                 self.board_cells[row][col] = CellValues.EMPTY
 
     def get_number_of_cells(self) -> int:
-        return len(self.board_cells)
+        return len(self.board_cells) * 3
+
+    def pickCell(self, selectCell: int, cellvalues: CellValues):
+        self.validate_emptycell_onboard(selectCell, cellvalues)
+        self.validate_board_range(selectCell)
+
+    def validate_emptycell_onboard(self, input_cell: int, cellvalues: CellValues):
+        row = (input_cell - 1) // 3
+        col = (input_cell - 1) % 3
+        if self.board_cells[row][col] == CellValues.EMPTY:
+            self.board_cells[row][col] = cellvalues
+        else:
+            raise InvalidBordCellException("Board cell is already picked")
+
+    def validate_board_range(self, input_cell: int):
+        if input_cell < 1 or input_cell > 9:
+            raise InvalidBordCellException("Board cell out of range")
+
+
+tic = TicTacToe()
+print(tic.get_number_of_cells())
