a9b4649af6c2fa56ca4e80a642570bb69139239c
Yisaterese <teresejosephyisa@gmail.com>
2024-03-12 08:17:33 +0100

    added code

--- tictactoe/player.py
+++ tictactoe/player.py
@@ -3,9 +3,19 @@ from tictactoe.tictactoe import TicTacToe
 
 
 class Player:
+    tictactoe = TicTacToe()
+
     def __init__(self, name):
         self.name = name
 
-    def make_move(self, input: int, cell_value: CellValues):
-        tictactoe = TicTacToe()
-        tictactoe.pickCell(input, cell_value)
+    def make_move(self, input_cell: int, cell_value: CellValues):
+        self.tictactoe.pickCell(input_cell, cell_value)
+
+    def display_board_cells(self):
+        print("_________________________")
+        for row in range(len(self.tictactoe.board_cells)):
+            print("|")
+            for col in self.tictactoe.board_cells:
+                print(self.tictactoe.board_cells[row][col])
+            print()
+            print("___________________________")
--- tictactoe/tictactoegame.py
+++ tictactoe/tictactoegame.py
@@ -1,9 +1,13 @@
+from tictactoe import tictactoe
 from tictactoe.cellvalues import CellValues
-from tictactoe.player import Player
+from tictactoe.invalidBordCellException import InvalidBordCellException
+from tictactoe.player import *
 from tictactoe.tictactoe import TicTacToe
 
+tictactoe = TicTacToe()
 
-def execute_tictactogame():
+
+def execute_tictactoe_game():
     player_X = Player("X")
     player_O = Player("O")
     current_player = player_X
@@ -12,3 +16,16 @@ def execute_tictactogame():
         return CellValues.X
     elif current_player == player_O:
         return CellValues.O
+
+
+print("     GAME START")
+while True:
+    input_cell = int(input())
+    try:
+        tictactoe.validate_board_range(input_cell)
+    except InvalidBordCellException as ex:
+        tictactoe.validate_board_range(input_cell)
+    else:
+
+
+
