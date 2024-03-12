9b5946f41e651dd134efc180d5e936d474aa624d
Yisaterese <teresejosephyisa@gmail.com>
2024-03-12 03:35:16 +0100

    added code

--- Bank/tictactoe/testtictactoe.py
+++ Bank/tictactoe/testtictactoe.py
@@ -1,3 +1,5 @@
+import pytest
+from Bank.tictactoe.invalidBordCellException import InvalidBordCellException
 from Bank.tictactoe.tictactoe import TicTacToe
 from Bank.tictactoe.cellvalues import CellValues
 
@@ -11,3 +13,33 @@ def test_cellValue_picked_equals_the_enum_type():
     tictactoe = TicTacToe()
     tictactoe.pickCell(9, CellValues.X)
     assert tictactoe.get_board_cellValue()[2][2] == CellValues.X
+
+
+def test_cellValue_picked_equals_is_out_of_raise_exception():
+    tictactoe = TicTacToe()
+    with pytest.raises(InvalidBordCellException) as info:
+        tictactoe.pickCell(10, CellValues.X)
+        assert tictactoe.get_board_cellValue()[2][2] == CellValues.X
+    assert str(info.value) == "Board cell out of range"
+
+
+def test_win_by_row_get_win():
+    tictactoe = TicTacToe()
+    tictactoe.pickCell(1, CellValues.X)
+    tictactoe.pickCell(4, CellValues.O)
+    tictactoe.pickCell(2, CellValues.X)
+    tictactoe.pickCell(5, CellValues.O)
+    tictactoe.pickCell(3, CellValues.X)
+    assert tictactoe.get_win_by_row() == CellValues.X
+
+
+def test_win_by_column_get_win():
+    tictactoe = TicTacToe()
+    tictactoe.pickCell(1, CellValues.X)
+    tictactoe.pickCell(2, CellValues.O)
+    tictactoe.pickCell(3, CellValues.X)
+    tictactoe.pickCell(5, CellValues.O)
+    tictactoe.pickCell(4, CellValues.X)
+    tictactoe.pickCell(8, CellValues.O)
+    assert tictactoe.get_win_by_column() == CellValues.O
+
--- Bank/tictactoe/tictactoe.py
+++ Bank/tictactoe/tictactoe.py
@@ -8,19 +8,16 @@ class TicTacToe:
                             [CellValues.EMPTY, CellValues.EMPTY, CellValues.EMPTY],
                             [CellValues.EMPTY, CellValues.EMPTY, CellValues.EMPTY]]
 
-    def populate_board_cells(self):
-        for row in range(0, 3):
-            for col in range(0, 3):
-                self.board_cells[row][col] = CellValues.EMPTY
-
     def get_number_of_cells(self) -> int:
         return len(self.board_cells) * 3
 
     def pickCell(self, selectCell: int, cell_values: CellValues):
-        self.validate_emptycell_onboard(selectCell, cell_values)
+
+        self.validate_empty_cell_onboard(selectCell, cell_values)
+        self.validate_player_turn(cell_values)
         self.validate_board_range(self, selectCell)
 
-    def validate_emptycell_onboard(self, input_cell: int, cell_values: CellValues):
+    def validate_empty_cell_onboard(self, input_cell: int, cell_values: CellValues):
         row = (input_cell - 1) // 3
         col = (input_cell - 1) % 3
         if self.board_cells[row][col] == CellValues.EMPTY:
@@ -36,6 +33,31 @@ class TicTacToe:
     def get_board_cellValue(self):
         return self.board_cells
 
+    def get_win_by_row(self):
 
-tic = TicTacToe()
-print(tic.get_number_of_cells())
+        is_win_by_first_row = self.board_cells[0][0] == self.board_cells[0][1] and self.board_cells[0][1] == self.board_cells[0][2]
+        is_win_by_second_row = self.board_cells[1][0] == self.board_cells[1][1] and self.board_cells[1][1] == self.board_cells[1][2]
+        is_win_by_third_row = self.board_cells[2][0] == self.board_cells[2][1] and self.board_cells[2][1] == self.board_cells[2][2]
+        if is_win_by_first_row:
+            return self.board_cells[0][0]
+
+        elif is_win_by_second_row:
+            return self.board_cells[1][0]
+
+        elif is_win_by_third_row:
+            return self.board_cells[2][0]
+
+    def get_win_by_column(self):
+
+        is_won_by_first_column = self.board_cells[0][0] and self.board_cells[1][0] and  self.board_cells[1][0] == self.board_cells[2][0]
+        is_won_by_second_column = self.board_cells[0][1] and self.board_cells[1][1] and self.board_cells[1][1] and self.board_cells[2][1]
+        is_won_by_third_column = self.board_cells[0][2] and self.board_cells[1][2] and self.board_cells[1][2] and self.board_cells[2][2]
+        if is_won_by_first_column or is_won_by_second_column or  is_won_by_third_column:
+            return CellValues.X
+        return CellValues.O
+
+    def validate_player_turn(self,input_cellvalue: CellValues):
+        if input_cellvalue == CellValues.X:
+            return CellValues.X
+        else:
+            return CellValues.O
